eb5279b0a4efd8fae5ae737437a38f4a05287bf6
Sakarona Bora <701017313@GoTVDSB.ca>
2025-01-09 09:37:38 -0500

    dragging working

--- final proj/Game1.cs
+++ final proj/Game1.cs
@@ -33,11 +33,28 @@ namespace final_proj
         SoundEffect vocal1, vocal2, vocal3, vocal4;
         SoundEffectInstance vocal1Instance, vocal2Instance, vocal3Instance, vocal4Instance;
 
-        //rect
+        //import the instuments 
+        Texture2D drum, guitar, keyboard, vocal;
+
+        //rect for charaters
         Rectangle keyboardRect = new Rectangle(100, 180, 220, 241);
         Rectangle guitarRect = new Rectangle(750, 180, 220, 241);
         Rectangle drumRect = new Rectangle(550, 180, 220, 241);
         Rectangle vocalRect = new Rectangle(320, 180, 220, 241);
+
+        //rect for instruments
+        Rectangle keyboardInRect = new Rectangle(100, 180, 60, 60);
+        Rectangle guitarInRect = new Rectangle(750, 180, 60, 60);
+        Rectangle drumInRect = new Rectangle(550, 180, 60, 60);
+        Rectangle vocalInRect = new Rectangle(320, 180, 60, 60);
+      
+
+
+        //bool is dragging instruments
+        bool isDraggingKeyboard, isDraggingGuitar, isDraggingDrum,isDraggingVocal;
+
+        //mousestate
+        MouseState currentMouseState, previousMouseState;
 
         public Game1()
         {
@@ -53,6 +70,12 @@ namespace final_proj
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.PreferredBackBufferWidth = 1079;
             _graphics.ApplyChanges();
+
+            //instruemnt dragging
+            isDraggingKeyboard = false;
+            isDraggingGuitar = false;
+            isDraggingDrum = false;
+            isDraggingVocal = false;
 
             base.Initialize();
         }
@@ -114,15 +137,65 @@ namespace final_proj
             guitarDanceTexture = Content.Load<Texture2D>("Charater/guitar/guitarDance");
             keyboardDanceTexture = Content.Load<Texture2D>("Charater/keyboard/keyDance");
             vocalDanceTexture = Content.Load<Texture2D>("Charater/vocal/vocalDance");
-            
+
+            //load instuments
+            keyboard = Content.Load<Texture2D>("keyboard");
+            guitar = Content.Load<Texture2D>("guitar");
+            drum = Content.Load<Texture2D>("drum");
+            vocal = Content.Load<Texture2D>("mic");
 
             // TODO: use this.Content to load your game content here
         }
 
+        // Returns true when a left click occurs -Mouse state
+        protected bool NewClick()
+        {
+            return currentMouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released;
+        }
+
         protected override void Update(GameTime gameTime)
         {
+            //add mouse state update
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+            //exit when esc is pressed
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            //Dragging the instruments anywhere
+            if (NewClick() && guitarInRect.Contains(currentMouseState.Position))
+            {
+                isDraggingGuitar = true;
+                this.Window.Title = "clicked";
+            }
+            else if (isDraggingGuitar && currentMouseState.LeftButton == ButtonState.Released)
+                isDraggingGuitar = false;
+            else if (isDraggingGuitar)
+                guitarInRect.Offset(currentMouseState.X - previousMouseState.X, currentMouseState.Y - previousMouseState.Y);
+            
+            if (NewClick() && keyboardInRect.Contains(currentMouseState.Position))
+                isDraggingKeyboard = true;
+            else if (isDraggingKeyboard && currentMouseState.LeftButton == ButtonState.Released)
+                isDraggingKeyboard = false;
+            else if (isDraggingKeyboard)
+                keyboardInRect.Offset(currentMouseState.X - previousMouseState.X, currentMouseState.Y - previousMouseState.Y);
+
+            if (NewClick() && drumInRect.Contains(currentMouseState.Position))
+                isDraggingDrum = true;
+            else if (isDraggingDrum && currentMouseState.LeftButton == ButtonState.Released)
+                isDraggingDrum = false;
+            else if (isDraggingDrum)
+                drumInRect.Offset(currentMouseState.X - previousMouseState.X, currentMouseState.Y - previousMouseState.Y);
+
+            if (NewClick() && vocalInRect.Contains(currentMouseState.Position))
+                isDraggingVocal = true;
+            else if (isDraggingVocal && currentMouseState.LeftButton == ButtonState.Released)
+                isDraggingVocal = false;
+            else if (isDraggingVocal)
+                vocalInRect.Offset(currentMouseState.X - previousMouseState.X, currentMouseState.Y - previousMouseState.Y);
+
+ 
+            
 
             //load all the sound effects and loop them
             guitar1Instance.IsLooped = true;
@@ -161,6 +234,7 @@ namespace final_proj
             //load random song to play and test
 
             vocal4Instance.Play();
+            drum3Instance.Play();
 
             //load the band members
             _spriteBatch.Draw(vocalTexture, vocalRect, Color.White);
@@ -168,10 +242,15 @@ namespace final_proj
             _spriteBatch.Draw(drumTexture, drumRect, Color.White);
             _spriteBatch.Draw(keyboardTexture, keyboardRect, Color.White);
 
-            _spriteBatch.End();
-
+            //load insturments
+            _spriteBatch.Draw(vocal, vocalInRect, Color.White);
+            _spriteBatch.Draw(guitar, guitarInRect, Color.White);
+            _spriteBatch.Draw(drum, drumInRect, Color.White);
+            _spriteBatch.Draw(keyboard, keyboardInRect, Color.White);
+            
             // TODO: Add your drawing code here
 
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
